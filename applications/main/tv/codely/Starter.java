a41a68a11faa3d55f84a5dae086b77c6f08caed2
Noelia Vigil <noeliav@empathy.co>
2022-10-11 11:20:31 +0200

    Update Starter.java

--- applications/main/tv/codely/Starter.java
+++ applications/main/tv/codely/Starter.java
@@ -2,6 +2,6 @@ package tv.codely;
 
 public class Starter {
     public static void main(String[] args) {
-        System.out.println("In Works!");
+        System.out.println("In Works!!");
     }
 }
